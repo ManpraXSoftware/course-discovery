d1993ed2d8e610a77f52202d4f02dc1b85a0829d
Ajamal Husain <dpython331@gmail.com>
2024-08-26 16:30:05 +0530

    added None if course language not selected

--- course_discovery/apps/extandedapi/views.py
+++ course_discovery/apps/extandedapi/views.py
@@ -257,13 +257,17 @@ class GetProgramCoursesDetail(APIView):
                     # converted_title = MultiLingualDiscovery.objects.filter(content_type='course',course_title=course.canonical_course_run).language(language).first()
                     converted_title = MultiLingualDiscovery.objects.filter(content_type='course', course_title__key=course.canonical_course_run.key).language(language).first()
                     data['converted_title'] = converted_title.title if converted_title else ''
-                    data['language'] = course.canonical_course_run.language.code
+                    try:
+                        data['language'] = course.canonical_course_run.language.code
+                    except:
+                        data['language'] = None
                     result["data"].append(data)
                 elif course.card_image_url:
                     courseKey = str('course')+course.card_image_url.split('asset')[1].split('type')[0][0:-1]
                     log.info("_________ course {} having card_image_url it's key is {}".format(course,courseKey))
                     data['key'] = courseKey
                     data['title'] = course.title
+                    data['language'] = None
                     # converted_title = MultiLingualDiscovery.objects.filter(content_type='course',course_title=CourseRun.objects.filter(key=courseKey).first()).language(language).first()
                     converted_title = MultiLingualDiscovery.objects.filter(content_type='course', course_title__key=courseKey).language(language).first()
                     data['converted_title'] = converted_title.title if converted_title else ''
