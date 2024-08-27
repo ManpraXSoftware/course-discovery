266b4c59e077f6c0d56902ab1b009eaebc88c438
Ajamal Husain <dpython331@gmail.com>
2024-08-27 17:09:37 +0530

    fixed recent access course issue

--- course_discovery/apps/extandedapi/views.py
+++ course_discovery/apps/extandedapi/views.py
@@ -529,7 +529,7 @@ class GetProgramTags2(APIView):
                             "course_name": course.title,
                             "converted_course_name":converted_course_name[0].title if converted_course_name.count() else course.title,
                             "block_id": resume_data[-1],
-                            "course_language":course.canonical_course_run.langauge.code
+                            "course_language":course.canonical_course_run.language.code
                         }
         # import pdb;pdb.set_trace()
         return Response(tags,status=status.HTTP_200_OK)
